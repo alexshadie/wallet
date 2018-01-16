b9df2ec49e4f39ae1423763ef231e662380456be
Alex Shadie <alex@astra.ws>
2018-01-17 00:34:59 +0300

    Local config

--- config/db.php
+++ config/db.php
@@ -1,14 +1,17 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=wallet',
-    'username' => 'wallet',
-    'password' => 'walletpwd',
-    'charset' => 'utf8',
+return array_merge(
+        [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=wallet',
+            'username' => '',
+            'password' => '',
+            'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
+            // Schema cache options (for production environment)
+            //'enableSchemaCache' => true,
+            //'schemaCacheDuration' => 60,
+            //'schemaCache' => 'cache',
+        ],
+        include __DIR__ . "/db-local.php"
+    );
